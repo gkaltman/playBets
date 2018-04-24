97c6a296a7d82fb6aed443c747e75240cf01a8b1
gkaltman <gkaltman@yahoo.com>
2018-04-24 21:56:08 +0300

    add README

--- src/main/java/communication/RequestExecutor.java
+++ src/main/java/communication/RequestExecutor.java
@@ -6,7 +6,7 @@ public interface RequestExecutor {
 
     /**
      * Execute a request and return a response.
-     * Inject all services, the subclasses will whatever service they want.
+     * Inject all services, the subclasses will use whatever service they want.
      */
     Response execute(AllServices allServices);
 }
